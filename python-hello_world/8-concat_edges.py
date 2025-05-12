09eb59af475ba9289a1026c0bad50df6e830be60
Andrew Perry <pezza76@hotmail.com>
2025-05-12 10:17:00 +1000

    task8

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str1[39:66] + str1[106:112] + str1[:6]
+print(str)
