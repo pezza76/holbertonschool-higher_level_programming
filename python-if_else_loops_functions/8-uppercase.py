846fa5a62a046ab0f185526526e967163c978c17
Andrew Perry <pezza76@hotmail.com>
2025-05-15 09:14:20 +1000

    task8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def islower(c):
-    for i in c:
-        if ord(i) >= ord('a') and ord(c) <= ord('z'):
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
             print("{}".format(chr(ord(i) - 32)), end="")
     else:
         print(i, end="")
