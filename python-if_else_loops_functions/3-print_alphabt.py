065b612e93d826ae79e3ffce5857cd4c42316dec
Andrew Perry <pezza76@hotmail.com>
2025-05-14 13:17:07 +1000

    task4

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(99):
-        print("{}  = 0x{:x}".format(i, i))
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
+
 
