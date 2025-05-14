4e8c6a42555fb78d8d635fc821c8b83443cda6aa
Andrew Perry <pezza76@hotmail.com>
2025-05-14 15:36:08 +1000

    task6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(10):
     for j in range(10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
-        elif i < j and j < 8:
+        elif i < j and i < 8:
             print("{}{}".format(i, j), end=", ")
