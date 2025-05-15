4cf3dd125da2fef936a6fdebaf008e0e2f5d0f3e
Andrew Perry <pezza76@hotmail.com>
2025-05-15 12:05:13 +1000

    task7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def islower(c):
     if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        print(chr(ord(c) - 32), end="")
+        return True
     else:
-        print(c, end="")
+        return False
