e0a3732964283d8a37be3f6cfe8013ae5009849f
Andrew Perry <pezza76@hotmail.com>
2025-05-20 20:54:43 +1000

    task1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if argc == 0:
         print("0 arguments")
     elif argc == 1:
-        print("1 argument: ")
+        print("1 argument:")
     else:
         print("{} arguments:".format(argc))
     
