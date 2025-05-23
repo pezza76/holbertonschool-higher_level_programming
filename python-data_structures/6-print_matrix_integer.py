92af8e867b5c68f1fee300d075b4c4a1007b4719
Andrew Perry <pezza76@hotmail.com>
2025-05-23 15:08:12 +1000

    task6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for index, j in enumerate(i):
             if index == len(i) - 1:
-               print("{:d}".format(j), end="")
+                print("{:d}".format(j), end="")
             else:
                 print("{:d}".format(j), end=" ")
         print()
