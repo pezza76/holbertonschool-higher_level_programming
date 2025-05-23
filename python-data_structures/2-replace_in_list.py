0ac3af513f3b5ce8138eb92b46a3d946eaa25cfc
Andrew Perry <pezza76@hotmail.com>
2025-05-23 11:44:49 +1000

    task2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,4 +4,4 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return None
     else:
-        my_list[idx] = my_list[element]
+        my_list[idx] = element
