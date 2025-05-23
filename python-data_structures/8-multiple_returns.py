958806b0a0c5a52d7937c628e56dbf939b9f946e
Andrew Perry <pezza76@hotmail.com>
2025-05-23 16:13:24 +1000

    task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     if sentence == "":
-        sentence[0] = None
-    tuple_a = (len(sentence), sentence[0])
-    return tuple_a
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
