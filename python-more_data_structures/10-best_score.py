004c38215ad91c9316e0fd1bcd2db54cb446c376
Andrew Perry <pezza76@hotmail.com>
2025-05-27 15:59:05 +1000

    task10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    return max(a_dictionary, key=a_dictionary.get)
+    if not a_dictionary:
+        return None
+    else:
+        return max(a_dictionary, key=a_dictionary.get)
