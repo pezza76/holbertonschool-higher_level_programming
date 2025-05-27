3842049ff497148338c182c8473a5de5e3dc8179
Andrew Perry <pezza76@hotmail.com>
2025-05-27 14:12:14 +1000

    task9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for i in a_dictionary:
-        new_dict[i] = a_dictionary[i]
+        new_dict[i] = a_dictionary[i] * 2
     return new_dict
