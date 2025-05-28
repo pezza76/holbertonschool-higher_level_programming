ef82e9da3317daeb51fe9ceab8a10039b50990e4
Andrew Perry <pezza76@hotmail.com>
2025-05-28 14:54:18 +1000

    task0

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(list_length)
+    for i in range(list_length):
         try:
             a = my_list_1[i]
             b = my_list_2[i]
