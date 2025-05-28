d763eca15d312f1780c49a0b2d418c6561bda63d
Andrew Perry <pezza76@hotmail.com>
2025-05-28 11:21:33 +1000

    task0

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(x, end=""))
+            count += 1
+        except (IndexError, TypeError):
+            break
+    print()
+    return count
