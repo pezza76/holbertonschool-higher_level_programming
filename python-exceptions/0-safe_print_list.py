37400f19f9dab594bc500c9077e24450f797a58f
Andrew Perry <pezza76@hotmail.com>
2025-05-28 10:12:29 +1000

    task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             count += 1
-    except IndexError:
-        break
+        except IndexError:
+            break
     print()
     return count
