dffadd15502e8b303d9732585206e3b6c075473a
Andrew Perry <pezza76@hotmail.com>
2025-05-29 11:02:33 +1000

    task0

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """An empty Square class."""
+    def __init__(self):
+        self.__size
