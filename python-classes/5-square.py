cf471fb3724a286a31f6f24abfe14e1c6ff0ea4b
Andrew Perry <pezza76@hotmail.com>
2025-05-29 13:17:35 +1000

    task0

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """Square class with a private size attribute."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+    
+    def my_print(self):
+        for _ in range(self.__size):
+            for i in range(self.__size):
+                print("#")
+            print()
