8314e8a18d93c2f26881d6361bba4e49cf1ea58f
Andrew Perry <pezza76@hotmail.com>
2025-06-01 11:58:51 +1000

    task1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -24,7 +24,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -32,4 +32,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
