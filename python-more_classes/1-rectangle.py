671de98eed72e6c04991d09a4acd76141a160a14
Andrew Perry <pezza76@hotmail.com>
2025-06-01 11:53:24 +1000

    task1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
     
