3f84a77342c18f619e0e7ceea0c9a401cabbd301
Andrew Perry <pezza76@hotmail.com>
2025-06-02 23:06:22 +1000

    task0

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""Defines an empty Rectangle class."""
+
+
+class Rectangle:
+    number_of_instances = 0
+
+    """Represents an empty rectangle."""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        number_of_instances +=1
+        
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return self.width * 2 + self.height * 2
+
+    def __str__(self):
+        if self.height == 0 or self.width == 0:
+            return ""
+        else:
+            return '\n'.join(['#' * self.width for _ in range(self.height)])
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        number_of_instances -=1
