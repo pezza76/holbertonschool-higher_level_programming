76c415a3122063586c3ebb1598963d96c2dbcc44
Andrew Perry <pezza76@hotmail.com>
2025-06-04 15:30:09 +1000

    task0

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""JSON representation of an object"""
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
