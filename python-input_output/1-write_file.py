1ac5453ba5f0e38ef14cea94fbd620094da27c9f
Andrew Perry <pezza76@hotmail.com>
2025-06-04 13:44:24 +1000

    task0

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""write text to a file"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
+  
