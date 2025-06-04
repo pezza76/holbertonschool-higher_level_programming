e73fddad744233f211c1dbdbf34326a9b3f593f7
Andrew Perry <pezza76@hotmail.com>
2025-06-04 15:53:54 +1000

    task0

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""object representation of an json string"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """function that returns an object (Python data structure) represented by a JSON string"""
+    return json.loads(my_obj)
