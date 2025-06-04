baef804e2f08bb376707ed563e8ac913115cabfb
Andrew Perry <pezza76@hotmail.com>
2025-06-04 13:10:09 +1000

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
- """function that reads a text file"""
+"""function that reads a text file"""
 
 def read_file(filename=""):
-
+    """function that reads a text file"""
     with open(filename) as f:
         print(f.read())
