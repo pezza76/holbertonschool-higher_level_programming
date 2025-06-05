2b44c0bf9ef135363be10234f3b5eef9620bf9ad
Andrew Perry <pezza76@hotmail.com>
2025-06-05 22:57:37 +1000

    task5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """function that writes an Object to a text file, using a JSON representation"""
+    """function that writes an Object to a text file"""
     with open(filename, 'w') as f:
         return json.dump(my_obj, f)
