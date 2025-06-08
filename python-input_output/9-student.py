451e070ef10577e7b74c49df176036e7aae34330
Andrew Perry <pezza76@hotmail.com>
2025-06-09 09:51:30 +1000

    task8

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,7 +2,7 @@
 """retrieves a dictionary representation of a Student instance"""
 class Student:
 
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
