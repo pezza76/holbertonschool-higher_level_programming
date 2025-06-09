6a0cb228130777b820796f1e221ad848020992e7
Andrew Perry <pezza76@hotmail.com>
2025-06-09 12:40:24 +1000

    task9

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""retrieves a dictionary representation of a Student instance"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None ):
+        """retrieves a dictionary representation of a Student instance"""
+        data = {}
+        if attrs=None:
+            return self.__dict__
+        else:
+            for i in attrs:
+            if hasattr(self, i):
+                value = getattr(self, i)
+                data[i] = value
+        return data
