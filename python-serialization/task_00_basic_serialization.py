0fb336b73b5c1c94ac17c3a1cfd9e0bc9e9ada3e
Andrew Perry <pezza76@hotmail.com>
2025-06-10 12:08:44 +1000

    task0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w') as f:
+        return json.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, 'r') as f:
+        return json.dump(f)
