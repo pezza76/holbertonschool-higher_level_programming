c35b6b6c574d607e40c3be8cca9cbfca3561d302
Andrew Perry <pezza76@hotmail.com>
2025-06-15 10:44:52 +1000

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Prints a sorted list"""
 
-class MyClass(list):
+
+class MyList(list):
     """Prints a list"""
     def print_sorted(self):
-        print(sorted(self)
+        print(sorted(selff))
