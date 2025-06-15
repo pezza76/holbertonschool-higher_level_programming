e86a5b8c8e368db71ec850df10a83c12f1bc2256
Andrew Perry <pezza76@hotmail.com>
2025-06-15 18:00:50 +1000

    task2

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class BaseGeometry:
+    """Create base class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{Pname} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
