83bdca549db14cdc1d2d6bf737fbcf28b7a8b1db
Andrew Perry <pezza76@hotmail.com>
2025-06-15 17:32:46 +1000

    task2

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class BaseGeometry:
+    """Create base class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
