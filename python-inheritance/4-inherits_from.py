f632f929897824c5a8080ea70db152a06000e577
Andrew Perry <pezza76@hotmail.com>
2025-06-15 15:48:14 +1000

    task2

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Checks class of object"""
-    return isinstance(obj, a_class) and is not type(a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
