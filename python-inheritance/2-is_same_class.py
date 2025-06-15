af5f76ce1590e324375d1c202369d93c521317f9
Andrew Perry <pezza76@hotmail.com>
2025-06-15 13:28:44 +1000

    task2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """Checks class of object"""
-    if type(obj) == type(a_class):
-        return type(obj) is a_class
-    else:
-        return False
+    return type(obj) is a_class
