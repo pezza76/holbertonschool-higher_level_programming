a24e8d9d18a850bfd2cced3b3d0292010cb115d2
Andrew Perry <pezza76@hotmail.com>
2025-06-16 18:25:18 +1000

    task1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -32,8 +32,8 @@ class Rectangle(Shape):
         return 2 * (self.width + self.height)
 
 def shape_info(shape):
-    print(f"{shape.area():.1f}")
-    print(f"{shape.perimeter():.1f}")
+    print(f"Area: {shape.area():.1f}")
+    print(f"Perimeter: {shape.perimeter():.1f}")
 
 circle = Circle(radius=5)
 shape_info(circle)
