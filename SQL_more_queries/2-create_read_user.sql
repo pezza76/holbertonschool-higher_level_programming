f6ae0b924fe0f0cff88711470109abf34bf74893
Andrew Perry <pezza76@hotmail.com>
2025-06-25 11:41:06 +1000

    task2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT TO 'user_0d_2'@'localhost' ON hbtn_0d_2.*;
+GRANT SELECT ON btn_0d_2.* TO 'user_0d_2'@'localhost';
