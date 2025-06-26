832ca39770fb25c1913db544a1c3aa38c3a0d20b
Andrew Perry <pezza76@hotmail.com>
2025-06-26 12:29:34 +1000

    task5

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 
  SELECT tv_shows.title, tv_show_genres.genre_id
  FROM tv_shows
- JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id;
+ JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id;
