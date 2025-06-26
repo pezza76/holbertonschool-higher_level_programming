8aac37b4077e62165e6374122c865f36dbe0e724
Andrew Perry <pezza76@hotmail.com>
2025-06-26 17:05:31 +1000

    task14

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- LIST ALL SHOWS AND GENRES
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY 1, 2;
