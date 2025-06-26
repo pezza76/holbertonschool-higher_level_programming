7041d2b32f99b59d773f84b1b914401daba7f8fe
Andrew Perry <pezza76@hotmail.com>
2025-06-26 12:23:01 +1000

    task5

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
  -- IMPORT A DATABASE
  USE hbtn_0d_tvshows;
  SELECT tv_shows.title, tv_show_genres.genre_id
- FROM title
+ FROM tv_shows
  JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id;
