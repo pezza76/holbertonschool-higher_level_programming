18ab9a12d17c212c3d6192b94467c5dbe4c3013a
Andrew Perry <pezza76@hotmail.com>
2025-06-27 17:38:40 +1000

    task0

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+)
+
+query = """
+SELECT name
+FROM states
+WHERE name LIKE'N%'
+ORDER BY states.id;
+"""
+
+cur = db.cursor()
+
+cur.execute(query)
+result = cur.fetchall()
+
+for i in result:
+    print(i)
