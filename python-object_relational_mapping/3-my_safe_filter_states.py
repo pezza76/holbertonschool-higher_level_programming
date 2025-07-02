dcdc86cd9c31d8cda79fb655d47f9de034678a44
Andrew Perry <pezza76@hotmail.com>
2025-07-02 14:05:23 +1000

    task2

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""safe input to defend against sql injection'"""
+
+import MySQLdb
+import sys
+
+# connect to a database
+db = MySQLdb.connect(
+    host='localhost',
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+)
+
+# create a cursor object
+cur = db.cursor()
+
+query = """
+SELECT *
+FROM states
+WHERE BINARY name = %s;
+"""
+
+# run a query
+cur.execute(query, (sys.argv[4],))
+
+# fetch results and save them to a variable
+result = cur.fetchall()
+
+for i in result:
+    print(i)
+
+cur.close()
+db.close()
