9bcc2723915fda8dfdb1a61d8ed524f505dabf4e
Andrew Perry <pezza76@hotmail.com>
2025-07-02 15:49:33 +1000

    task5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -4,33 +4,34 @@
 import MySQLdb
 import sys
 
-# connect to a database
-db = MySQLdb.connect(
-    host='localhost',
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
+if __name__ == '__main__':
+    # connect to a database
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-# create a curso object
-cur = db.cursor()
+    # create a curso object
+    cur = db.cursor()
 
-# create the sql query
-query = """
-select cities.name
-from cities
-join states on cities.state_id = states.id
-where states.name = %s
-order by cities.id;
-"""
-# execute the query
-cur.execute(query, (sys.argv[4],))
+    # create the sql query
+    query = """
+    select cities.name
+    from cities
+    join states on cities.state_id = states.id
+    where states.name = %s
+    order by cities.id;
+    """
+    # execute the query
+    cur.execute(query, (sys.argv[4],))
 
-#fetch the results and save to a variable
-result = cur.fetchall()
+    #fetch the results and save to a variable
+    result = cur.fetchall()
 
-for i in result:
-    print(i)
+    for i in result:
+        print(i)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
