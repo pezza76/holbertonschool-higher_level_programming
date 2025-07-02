a2c8b5f2ccdb627ea3b84dc9b0d3801f805f89d7
Andrew Perry <pezza76@hotmail.com>
2025-07-02 12:45:26 +1000

    task1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that lists all states with a name starting with 'N' from the database."""
+"""Module that lists all states with a name starting with 'N'"""
 
 import MySQLdb
 import sys
