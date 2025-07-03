a6d4d42d51626cbdb09e1b9005002a1694a168cb
Andrew Perry <pezza76@hotmail.com>
2025-07-03 16:39:02 +1000

    task13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Delete states containing the letter 'a'"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+            f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # find states containing the letter a
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    
+    # delete these states
+    for state in states:
+        session.delete(state)
+
+    session.commit()
