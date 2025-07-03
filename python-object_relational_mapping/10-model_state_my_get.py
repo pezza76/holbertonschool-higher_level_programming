d4f31e663c53681d2128afcdbb28e71e3e14a97d
Andrew Perry <pezza76@hotmail.com>
2025-07-03 16:09:38 +1000

    task9

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""python file that prints all state entered by user"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+            f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # create a query
+    state = session.query(State).filter_by(name=sys.argv[4]).first()
+
+    # print result
+    if state:
+        print(state.id)
+    else:
+        print('Nothing')
