09cf74971c8a91ad4571991ba03818b231bef6ac
Andrew Perry <pezza76@hotmail.com>
2025-07-03 14:17:50 +1000

    task6

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""python file that contains the class definition of a State"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
+import sys
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+
+# create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+states = session.query(State).all()
+
+for state in states:
+    print(state)
+
