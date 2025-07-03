55130762dd5cc3d9c5104fdb2515d6e7deff8c80
Andrew Perry <pezza76@hotmail.com>
2025-07-03 14:48:32 +1000

    task7

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,25 @@
+#!/usr/bin/python3
+"""python file that contains the class definition of a State"""
+
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import sys
 
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+    engine = create_engine(
+            f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
 
-engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# create a session
-Session = sessionmaker(bind=engine)
-session = Session()
+    # create a query
+    first = session.query(State).first()
 
-# create a query
-first = session.query(State).first()
-
-for state in first:
-    print(f"{state.id}: {state.name}")
+    for state in first:
+        print(f"{state.id}: {state.name}")
