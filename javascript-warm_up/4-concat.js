824262692c4aaae1909afff3b3eb2427d777515d
Andrew Perry <pezza76@hotmail.com>
2025-07-24 15:30:43 +1000

    task4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2)
-let arg1 = args[0]
-let arg2 = args[1]
-console.log(`${arg1} is ${arg2}`)
+const args = process.argv.slice(2);
+const arg1 = args[0];
+const arg2 = args[1];
+console.log(`${arg1} is ${arg2}`);
