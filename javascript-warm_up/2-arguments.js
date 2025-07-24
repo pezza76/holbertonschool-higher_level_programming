a681071982611177d45c690aeab8ade30d13021b
Andrew Perry <pezza76@hotmail.com>
2025-07-24 13:59:50 +1000

    task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@
  
  if (len === 0) {
     console.log("No argument");
- } else if (len === 1) {
+ } else if (len === 1) { 
     console.log("Argument found");
  } else {
     console.log("Arguments found");
