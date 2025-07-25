3e7f4637c8d9e6788178470fa6076d0ae2ecdb2e
Andrew Perry <pezza76@hotmail.com>
2025-07-25 14:28:34 +1000

    task8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,9 @@ const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
 
 function add(a, b) {
-    console.log(a + b);
+    for (let i = 0; i < 1; i++) {
+        console.log(a + b);
+    }
 }
 
 add(a, b);
