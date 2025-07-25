a8667038b0b38a024c91d72203407eec2c638281
Andrew Perry <pezza76@hotmail.com>
2025-07-26 08:57:39 +1000

    task10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const num = Number(process.argv[2])
+
+if (!Number.isInteger(num)) {
+    console.log(1)
+} else {
+    function factorial (num) {
+        if (num === 0) {
+            return 1;
+        } 
+        return num * factorial (num - 1);
+    }
+    console.log(factorial(num));
+}
