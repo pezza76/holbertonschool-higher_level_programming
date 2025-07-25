9851f1a50946c33d7497428dc3bb64cb6004b6f9
Andrew Perry <pezza76@hotmail.com>
2025-07-25 23:50:52 +1000

    task8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,8 +2,8 @@
 sizeOfSquare = Number(process.argv[2]);
 
 if (!Number.isInteger(sizeOfSquare)) {
-    console.log('Missing size')
+  console.log('Missing size');
 }
 for (let i = 0; i < sizeOfSquare; i++) {
-    console.log('X'.repeat(sizeOfSquare))
+  console.log('X'.repeat(sizeOfSquare));
 }
