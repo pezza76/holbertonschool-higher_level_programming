e0bf4eed1f50bda0bad09f67721a51660bf50178
Andrew Perry <pezza76@hotmail.com>
2025-07-27 11:04:26 +1000

    task11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,10 @@
 const nums = process.argv.slice(2).map(Number);
 
 if (process.argv[2] === undefined) {
-    console.log(0);
+  console.log(0);
+} else if (nums.length === 1) {
+  console.log(0);
 } else {
-const sort = nums.sort((a, b) => a - b);
-console.log(sort[sort.length - 2]);
+  const sort = nums.sort((a, b) => a - b);
+  console.log(sort[sort.length - 2]);
 }
