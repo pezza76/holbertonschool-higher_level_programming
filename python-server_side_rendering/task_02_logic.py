a6771d0fd5cdfb6a37e4601cc8605713b46b68d7
Andrew Perry <pezza76@hotmail.com>
2025-07-29 10:06:45 +1000

    task2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -19,6 +19,6 @@ def about():
 def items():
     with open('items.json') as f:
         data = json.load(f)
-    return render_template('items.html', items = data['items'])
+    return render_template('items.html', items = data.get['items', [])
 if __name__ == "__main__":
     app.run(debug=True)
