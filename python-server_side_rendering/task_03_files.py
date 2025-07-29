9a71303b334da5d501816099d2095c9272fb6a62
Andrew Perry <pezza76@hotmail.com>
2025-07-29 13:15:47 +1000

    task3

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,41 @@
+
+from flask import Flask, render_template, json, request
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        data = json.load(f)
+    return render_template('items.html', items = data.get('items', []))
+
+@app.route('/products')
+def products():
+    if request.args.get('source') == 'json':
+        with open('products.json') as f:
+            data = json.load(f)
+        return render_template('product_display.html', items = data)
+    
+    elif request.args.get('source') == 'csv':
+        with open('products.csv') as f:
+            reader = csv.DictReader(f)
+            data = list(reader)
+        return render_template('product_display.html', items = data)
+    
+    return "Invalid source", 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
