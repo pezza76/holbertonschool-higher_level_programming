beba42044a96ce64fe825b2d74f3455dad118217
Andrew Perry <pezza76@hotmail.com>
2025-07-30 12:06:54 +1000

    task9

---
+++ python-everything_is_object/test.py
@@ -0,0 +1,6 @@
+s1 = "Best Schooljlkjlk;jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfdsaaaaaaaaaaaaaaaaaaaaaaa"
+s2 = "Best Schooljlkjlk;jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfdsaaaaaaaaaaaaaaaaaaaaaaa"
+print(s1 is s2)
+
+print(id(s1))
+print(id(s2))
